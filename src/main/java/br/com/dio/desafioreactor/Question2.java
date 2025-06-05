a477cfde16c749b3d30c955d8d965ab5fa228ab6
Juliana <140960908+ajulieazvdo@users.noreply.github.com>
2025-06-04 21:47:38 -0300

    feat: resolve todos os desafios

--- src/main/java/br/com/dio/desafioreactor/Question2.java
+++ src/main/java/br/com/dio/desafioreactor/Question2.java
@@ -9,6 +9,10 @@ public class Question2 {
     /*
     Recebe uma lista de usuários e retorna a quantos usuários admin tem na lista
      */
-    public Mono<Long> countAdmins(final List<User> users){}
+    public Mono<Long> countAdmins(final List<User> users){
+        return Mono.just(users)
+            .map(u -> u.stream().filter(User::isAdmin).count())
+            .doOnNext(System.out::println);
+    }
 
 }
--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -4,10 +4,18 @@ import reactor.core.publisher.Mono;
 
 public class Question3 {
 
-    /*
-    Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
-    (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
-     */
-    public Mono<Void> userIsValid(final User user){}
+  /*
+  Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
+  (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
+   */
+  public Mono<Void> userIsValid(final User user) {
+    return Mono.just(user)
+        .flatMap(user1 -> {
+          if (user1 == null || user1.password().length() < 8) {
+            return Mono.error(new IllegalArgumentException("Senha invalida"));
+          }
+          return Mono.empty();
+        });
+  }
 
 }
